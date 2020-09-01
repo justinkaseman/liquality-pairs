19e77df0dd654fdd6640ad2195840c79288b0974
Justin Kaseman <justinkaseman@live.com>
2020-09-01 15:30:12 -0700

    feat: Sets up global theme for light/dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React from "react";
-import TradingPairsPage from "./components/TradingPairsPage";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { Header, TradingPairs } from "./components";
+import { useGlobalState } from "./context";
 
 function App() {
-  return <TradingPairsPage />;
+  const { theme } = useGlobalState();
+  const currentTheme = createMuiTheme({
+    palette: {
+      type: theme,
+    },
+  });
+  return (
+    <ThemeProvider theme={currentTheme}>
+      <div className="App">
+        <Header />
+        <TradingPairs />
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/components/index.tsx
@@ -0,0 +1,5 @@
+import Header from "./Header";
+import TradingPairs from "./TradingPairs";
+
+export { Header };
+export { TradingPairs };

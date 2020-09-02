2b0aa382086c3ab44c779676ce88de9df464e4a6
Justin Kaseman <justinkaseman@live.com>
2020-09-02 15:22:26 -0700

    feat: Add constants for cryptocurrency conversion rates

---
+++ src/shared/constants/tokens.tsx
@@ -0,0 +1,12 @@
+interface Tokens {
+  [key: string]: number;
+}
+
+export const tokens: Tokens = {
+  DAI: 1000000000000000000,
+  ETH: 1000000000000000000,
+  BTC: 100000000,
+  WBTC: 100000000,
+  USDC: 1000000,
+  USDT: 1000000,
+};

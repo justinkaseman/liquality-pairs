60a709b2d7c36447687a2cd55ac9670b1ea661f3
Justin Kaseman <justinkaseman@live.com>
2020-09-02 12:49:13 -0700

    feat: Cryptocurrency symbols component

---
+++ src/components/CryptoIcon.tsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+
+export default function CryptoIcon({ ticker }: { ticker: any }) {
+  let [icon, setIcon] = useState("");
+
+  useEffect(() => {
+    const importIcon = async () => {
+      try {
+        let iconImport = await import(
+          `../../node_modules/cryptocurrency-icons/32/color/${ticker.toLowerCase()}.png`
+        );
+        setIcon(iconImport.default);
+      } catch {
+        let fallbackImport = await import(
+          `../../node_modules/cryptocurrency-icons/32/color/generic.png`
+        );
+        setIcon(fallbackImport.default);
+      }
+    };
+    importIcon();
+  }, [ticker]);
+
+  return <img alt={`${ticker} symbol`} src={icon} />;
+}
